714bb5d7da2db61789f57ad2290540207874fefa
Jenish Rajbasnet <np03cs4a240300@heraldcollege.edu.np>
2025-12-29 10:56:39 +0545

    changed

--- kkk.java
+++ kkk.java
@@ -1,5 +1,10 @@
 package hjhg;
 
 public class kkk {
+	public static void main (String [] args) {
+		System.out.println("this is our app");
+		System.out.println("updates");
+		System.out.println("aldo");
+	}
 
 }
